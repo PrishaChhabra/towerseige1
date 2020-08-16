cc4a0837cf01b8852257b2ffddf1f5695d3213a7
PrishaChhabra <66011995+PrishaChhabra@users.noreply.github.com>
2020-08-16 03:51:41 -0400

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,30 @@
+class Block{
+    constructor(x, y, width, height,color) {
+      var options = {
+           
+          'restitution':0,
+          'friction':0,
+          'density':1.0
+         
+      }
+    this.color=color;
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      strokeWeight(3);
+      //stroke("yellow");
+      fill(this.color);
+      rect(0, 0, this.width, this.height,this.color);
+      pop();
+    }
+  }
---
+++ Slingshot.js
@@ -0,0 +1,46 @@
+class Slingshot{
+    constructor(bodyA,pointB){
+    var options={
+        length:10,
+        stiffness:0.04,
+        bodyA:bodyA,
+        pointB:pointB
+    
+    }
+    this.slingshot=Constraint.create(options);
+    this.pointB=pointB;
+    World.add(world,this.slingshot);
+    }
+
+    fly(){
+        this.slingshot.bodyA=null;
+        
+        
+        }
+
+
+    display(){
+        if(this.slingshot.bodyA){
+        push();
+    var pointA= this.slingshot.bodyA.position;
+    var pointB= this.slingshot.pointB;
+    strokeWeight(4);
+    line(pointA.x,pointA.y,pointB.x,pointB.y);
+        
+    pop();
+        }
+    
+    
+    }
+    
+    
+    
+    
+    
+    }
+
+
+
+  
+    
+      
